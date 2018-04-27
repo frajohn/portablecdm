9e6d5fa1861ccd197160c127ffbddc1d1584314a
John <frajohn@student.chalmers.se>
2018-04-27 15:48:37 +0200

    Revert "Changed index file in components"
    This reverts commit dc1aa87b816bfd1740147d7d80db425e99d48b1a.

--- App/components/send-portcall-view/index.js
+++ App/components/send-portcall-view/index.js
@@ -1,4 +1,3 @@
-
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { 
@@ -44,8 +43,11 @@ import colorScheme from '../../config/colors';
 import { createPortCallMessageAsObject, objectToXml } from '../../util/xmlUtils';
 import { cleanURN } from '../../util/stringUtils';
 import { getDateTimeString } from '../../util/timeservices';
-
+<<<<<<< HEAD
+=======
 import { hasComment, promptOpposite } from '../../config/instances';
+>>>>>>> 7db59deffae50f7ed04d9ac475b6bdca6e768c8d
+
 
 let navBackTimer      = null;
 let initRedirectTimer = null;
